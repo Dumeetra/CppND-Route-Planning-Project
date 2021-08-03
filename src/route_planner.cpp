9542b9c8d6aaeaef9cbe1066d1dbd231ea6e0e41
Dumeetra <86459746+Dumeetra@users.noreply.github.com>
2021-08-03 17:47:25 +0300

    Update route_planner.cpp

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,4 +1,4 @@
-xs#include "route_planner.h"
+#include "route_planner.h"
 #include <algorithm>
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
